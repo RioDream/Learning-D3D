83c44f2d3381d48898025927f3c63893cadbf339
rio <ao.li@nlpr.ia.ac.cn>
2014-03-27 11:12:41 +0800

    modify it to get mountains

--- 2.Direct3DApp1/Direct3DApp1/CubeRenderer.cpp
+++ 2.Direct3DApp1/Direct3DApp1/CubeRenderer.cpp
@@ -27,11 +27,11 @@ void CubeRenderer::CreateDeviceResources()
 	auto createVSTask = loadVSTask.then([this](Platform::Array<byte>^ fileData) {
 		DX::ThrowIfFailed(
 			m_d3dDevice->CreateVertexShader(
- 				fileData->Data,
-				fileData->Length,
-				nullptr,
-				&m_vertexShader
-				)
+			fileData->Data,
+			fileData->Length,
+			nullptr,
+			&m_vertexShader
+			)
 			);
 
 		const D3D11_INPUT_ELEMENT_DESC vertexDesc[] = 
@@ -42,12 +42,12 @@ void CubeRenderer::CreateDeviceResources()
 
 		DX::ThrowIfFailed(
 			m_d3dDevice->CreateInputLayout(
-				vertexDesc,
-				ARRAYSIZE(vertexDesc),
-				fileData->Data,
-				fileData->Length,
-				&m_inputLayout
-				)
+			vertexDesc,
+			ARRAYSIZE(vertexDesc),
+			fileData->Data,
+			fileData->Length,
+			&m_inputLayout
+			)
 			);
 	});
 
@@ -55,37 +55,94 @@ void CubeRenderer::CreateDeviceResources()
 	auto createPSTask = loadPSTask.then([this](Platform::Array<byte>^ fileData) {
 		DX::ThrowIfFailed(
 			m_d3dDevice->CreatePixelShader(
-				fileData->Data,
-				fileData->Length,
-				nullptr,
-				&m_pixelShader
-				)
+			fileData->Data,
+			fileData->Length,
+			nullptr,
+			&m_pixelShader
+			)
 			);
 
 		CD3D11_BUFFER_DESC constantBufferDesc(sizeof(ModelViewProjectionConstantBuffer), D3D11_BIND_CONSTANT_BUFFER);
 		DX::ThrowIfFailed(
 			m_d3dDevice->CreateBuffer(
-				&constantBufferDesc,
-				nullptr,
-				&m_constantBuffer
-				)
+			&constantBufferDesc,
+			nullptr,
+			&m_constantBuffer
+			)
 			);
 	});
+
+
+
+
+
+
+
 
 	//利用上两步的像素着色器和顶点着色器Task，创建cubeTask
 	auto createCubeTask = (createPSTask && createVSTask).then([this] () {
 		//顶点位置和颜色矩阵
-		VertexPositionColor cubeVertices[] = 
-		{
-			{XMFLOAT3(-0.5f, -0.5f, -0.5f), XMFLOAT3(0.0f, 0.0f, 0.0f)},
-			{XMFLOAT3(-0.5f, -0.5f,  0.5f), XMFLOAT3(0.0f, 0.0f, 1.0f)},
-			{XMFLOAT3(-0.5f,  0.5f, -0.5f), XMFLOAT3(0.0f, 1.0f, 0.0f)},
-			{XMFLOAT3(-0.5f,  0.5f,  0.5f), XMFLOAT3(0.0f, 1.0f, 1.0f)},
-			{XMFLOAT3( 0.5f, -0.5f, -0.5f), XMFLOAT3(1.0f, 0.0f, 0.0f)},
-			{XMFLOAT3( 0.5f, -0.5f,  0.5f), XMFLOAT3(1.0f, 0.0f, 1.0f)},
-			{XMFLOAT3( 0.5f,  0.5f, -0.5f), XMFLOAT3(1.0f, 1.0f, 0.0f)},
-			{XMFLOAT3( 0.5f,  0.5f,  0.5f), XMFLOAT3(1.0f, 1.0f, 1.0f)},
-		};
+		//#LA
+		const int xRange = 128;
+		const int zRange = 128;
+		const float dx = 1.0f;
+		const XMFLOAT3 WHITE(1.0f,1.0f,1.0f);
+		const XMFLOAT3 BEACH_SAND(1.0f, 0.96f, 0.62f);  
+		const XMFLOAT3 LIGHT_YELLOW_GREEN(0.48f, 0.77f, 0.46f);  
+		const XMFLOAT3 DARK_YELLOW_GREEN(0.1f, 0.48f, 0.19f);  
+		const XMFLOAT3 DARKBROWN(0.45f, 0.39f, 0.34f); 
+		//VertexPositionColor sceneVertices[xRange*zRange];
+		//for(int row=0;row<zRange; ++row)  
+		//{  
+		//	float zPos = row*dx;  
+		//  
+		//   for(int col=0;col<xRange; ++col)  
+		//   {  
+		//        float xPos = col*dx;  
+		//        float yPos = 0.3f *(zPos*sinf(0.1f*xPos) + xPos*cosf(0.1*zPos));  
+		//        sceneVertices[xRange*row + col].pos = XMFLOAT3(xPos, yPos,zPos);  
+		//  
+		//        if(yPos <-10.0f)  
+		//            sceneVertices[xRange*row + col].color =BEACH_SAND;  
+		//        else if (yPos <5.0f)  
+		//            sceneVertices[xRange*row + col].color =LIGHT_YELLOW_GREEN;  
+		//        else if (yPos <10.0f)  
+		//            sceneVertices[xRange*row + col].color =DARK_YELLOW_GREEN;  
+		//        else if (yPos <12.0f)  
+		//            sceneVertices[xRange*row+ col].color = DARKBROWN;  
+		//        else  
+		//            sceneVertices[xRange*row + col].color =WHITE;  
+		//   }  
+		//}  
+
+		/*VertexPositionColor cubeVertices[xRange*zRange];
+
+		memcpy(cubeVertices,sceneVertices,xRange*zRange);*/
+
+		VertexPositionColor cubeVertices[xRange*zRange];
+		for(int row=0;row<zRange; ++row)  
+		{  
+			float zPos = row*dx;  
+
+			for(int col=0;col<xRange; ++col)  
+			{  
+				float xPos = col*dx;  
+				float yPos = 0.3f *(zPos*sinf(0.1f*xPos) + xPos*cosf(0.1*zPos));  
+				cubeVertices[xRange*row + col].pos = XMFLOAT3(xPos, yPos,zPos);  
+
+				//根据山峰的高度设置不同的颜色，而这些颜色之间是渐变的
+				if(yPos <-10.0f)  
+					cubeVertices[xRange*row + col].color =BEACH_SAND;  
+				else if (yPos <5.0f)  
+					cubeVertices[xRange*row + col].color =LIGHT_YELLOW_GREEN;  
+				else if (yPos <10.0f)  
+					cubeVertices[xRange*row + col].color =DARK_YELLOW_GREEN;  
+				else if (yPos <12.0f)  
+					cubeVertices[xRange*row+ col].color = DARKBROWN;  
+				else  
+					cubeVertices[xRange*row + col].color =WHITE;  
+			}  
+		}  
 
 		D3D11_SUBRESOURCE_DATA vertexBufferData = {0};
 		vertexBufferData.pSysMem = cubeVertices;
@@ -94,33 +151,54 @@ void CubeRenderer::CreateDeviceResources()
 		CD3D11_BUFFER_DESC vertexBufferDesc(sizeof(cubeVertices), D3D11_BIND_VERTEX_BUFFER);
 		DX::ThrowIfFailed(
 			m_d3dDevice->CreateBuffer(
-				&vertexBufferDesc,
-				&vertexBufferData,
-				&m_vertexBuffer
-				)
+			&vertexBufferDesc,
+			&vertexBufferData,
+			&m_vertexBuffer
+			)
 			);
 
 		//三角面片
-		unsigned short cubeIndices[] = 
-		{
-			0,2,1, // -x
-			1,2,3,
+		/*unsigned short Indices[3*2*(xRange-1)*(zRange-1)];  
 
-			4,5,6, // +x
-			5,7,6,
+		int tempIndice = 0;  
+		for(int row=0;row<(zRange-1); ++row)  
+		{  
+		for(int col=0;col<(xRange-1); ++col)  
+		{  
+		Indices[tempIndice] = xRange*row + col;  
+		Indices[tempIndice+1] = xRange*row +col + 1;  
+		Indices[tempIndice+2] = xRange*(row+1)+ col;  
 
-			0,1,5, // -y
-			0,5,4,
+		Indices[tempIndice+3] = xRange*(row+1)+ col;  
+		Indices[tempIndice+4] = xRange*row +col + 1;  
+		Indices[tempIndice+5] = xRange*(row+1)+ col + 1;  
 
-			2,6,7, // +y
-			2,7,3,
+		tempIndice += 6;  
+		}  
+		}  
 
-			0,4,6, // -z
-			0,6,2,
+		unsigned short cubeIndices[3*2*(xRange-1)*(zRange-1)];
+		memcpy(cubeIndices,Indices,3*2*(xRange-1)*(zRange-1));*/
 
-			1,3,7, // +z
-			1,7,5,
-		};
+		//三角面片
+		unsigned short cubeIndices[3*2*(xRange-1)*(zRange-1)]; 
+		int tempIndice = 0;  
+		for(int row=0;row<(zRange-1); ++row)  
+		{  
+			for(int col=0;col<(xRange-1); ++col)  
+			{  
+				cubeIndices[tempIndice] = xRange*row + col;  
+				cubeIndices[tempIndice+1] = xRange*row +col + 1;  
+				cubeIndices[tempIndice+2] = xRange*(row+1)+ col;  
+
+				cubeIndices[tempIndice+3] = xRange*(row+1)+ col;  
+				cubeIndices[tempIndice+4] = xRange*row +col + 1;  
+				cubeIndices[tempIndice+5] = xRange*(row+1)+ col + 1;  
+
+				tempIndice += 6;  
+			}  
+		}  
+
 
 		m_indexCount = ARRAYSIZE(cubeIndices);
 
@@ -131,10 +209,10 @@ void CubeRenderer::CreateDeviceResources()
 		CD3D11_BUFFER_DESC indexBufferDesc(sizeof(cubeIndices), D3D11_BIND_INDEX_BUFFER);
 		DX::ThrowIfFailed(
 			m_d3dDevice->CreateBuffer(
-				&indexBufferDesc,
-				&indexBufferData,
-				&m_indexBuffer
-				)
+			&indexBufferDesc,
+			&indexBufferData,
+			&m_indexBuffer
+			)
 			);
 	});
 
@@ -154,6 +232,8 @@ void CubeRenderer::CreateWindowSizeDependentResources()
 	float aspectRatio = m_windowBounds.Width / m_windowBounds.Height;
 	float fovAngleY = 70.0f * XM_PI / 180.0f;
 
+
+
 	// 请注意，此处后乘 m_orientationTransform3D 矩阵，
 	// 以正确确定场景的方向，使之与显示方向匹配。
 	// 对于交换链的目标位图进行的任何绘制调用
@@ -162,16 +242,16 @@ void CubeRenderer::CreateWindowSizeDependentResources()
 	XMStoreFloat4x4(
 		&m_constantBufferData.projection,
 		XMMatrixTranspose(
-			XMMatrixMultiply(
-				XMMatrixPerspectiveFovRH(
-					fovAngleY,
-					aspectRatio,
-					0.01f,
-					100.0f
-					),
-				XMLoadFloat4x4(&m_orientationTransform3D)
-				)
-			)
+		XMMatrixMultiply(
+		XMMatrixPerspectiveFovRH( 
+		fovAngleY,
+		aspectRatio,
+		0.01f,
+		1000.0f
+		),
+		XMLoadFloat4x4(&m_orientationTransform3D)
+		)
+		)
 		);
 }
 
@@ -180,6 +260,19 @@ void CubeRenderer::Update(float timeTotal, float timeDelta)
 {
 	(void) timeDelta; // 未使用的参数。
 
+	float flyPos = timeTotal * 5;
+	XMVECTOR eye =XMVectorSet(flyPos, 30.0f, flyPos, 0.0f);
+	XMVECTOR at =XMVectorSet(50.0f + flyPos, -0.1f, 50.0f + flyPos, 0.0f);
+
+	//XMVECTOR eye =XMVectorSet(0, 30.0f, 0, 0.0f);
+	//XMVECTOR at =XMVectorSet(50.0f, -0.1f, 50.0f, 0.0f);
+	XMVECTOR up =XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+
+	XMStoreFloat4x4(&m_constantBufferData.view,XMMatrixTranspose(XMMatrixLookAtRH(eye, at, up)));
+	XMStoreFloat4x4(&m_constantBufferData.model,XMMatrixTranspose(XMMatrixRotationY(0.0f)));
+
+	/*
 	XMVECTOR eye = XMVectorSet(0.0f, 0.7f, 1.5f, 0.0f); //眼睛所在位置
 	XMVECTOR at = XMVectorSet(0.0f, -0.1f, 0.0f, 0.0f); //眼睛观察方向
 	XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f); //上方向
@@ -187,6 +280,7 @@ void CubeRenderer::Update(float timeTotal, float timeDelta)
 
 	XMStoreFloat4x4(&m_constantBufferData.view, XMMatrixTranspose(XMMatrixLookAtRH(eye, at, up)));
 	XMStoreFloat4x4(&m_constantBufferData.model, XMMatrixTranspose(XMMatrixRotationY(timeTotal * XM_PIDIV4)));
+	*/
 }
 
 //Render方法实现最终图像的绘制，其主要就是设置各个功能的缓冲区地址。
